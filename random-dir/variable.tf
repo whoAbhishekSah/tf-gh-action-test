843ab4f262c927b50c10a933378ec0d321df0a17
Abhishek Sah <abhi.sah.97@gmail.com>
2023-07-20 14:59:56 +0530

    add some random files

---
+++ random-dir/variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  type        = string
+  description = "AWS Region where all resources will be provisioned"
+  default     = "us-east-2"
+}
+
+variable "team" {
+  type        = string
+  description      = "Group under which resources will be deployed"
+  default     = "platform"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+  default     = "development"
+}
